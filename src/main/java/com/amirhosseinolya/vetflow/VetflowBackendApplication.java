93eb580962f4997b0d8e8e35b9a249c9eba347d3
amirholya01 <amirholya01@gmail.com>
2026-08-30 12:30:00 +0330

    chore: initialize Spring Boot Maven project

---
+++ src/main/java/com/amirhosseinolya/vetflow/VetflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amirhosseinolya.vetflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetflowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amirhosseinolya/vetflow/VetflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amirhosseinolya.vetflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetflowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
